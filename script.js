a04d688d7a63fa68c06d4f00c0195b684e640b4d
jparrker <102701917+jparrker@users.noreply.github.com>
2023-11-23 10:29:55 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function openMenu(event, menuName) {
+  let menuArray, i, tablinks;
+  menuArray = document.getElementsByClassName("menu");
+  for (i = 0; i < menuArray.length; i++) {
+    menuArray[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active-tab");
+  }
+
+  document.getElementById(menuName).style.display = "block";
+  event.currentTarget.classList.add("active-tab");
+}
+
+document.getElementById("mainLink").click();
